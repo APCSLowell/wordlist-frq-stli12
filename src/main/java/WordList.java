3172d6961f195957afeaf29d4c18b37bdfafb0a6
stli12 <143028001+stli12@users.noreply.github.com>
2024-04-03 12:11:09 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -12,7 +12,7 @@ public class WordList
   {
    int amount = 0;
     for(int i =0; i < myList.size(); i++){
-        if(myList.get(i).size() == len)
+        if(myList.get(i).length() == len)
           amount ++;
     }
     return amount;
@@ -21,7 +21,7 @@ public class WordList
   public void removeWordsOfLength(int len)
   {
     for(int j = 0; j < myList.size(); j ++){
-      if(myList.get(j).size() == len){
+      if(myList.get(j).length() == len){
         myList.remove(j);
         j--;
       }
